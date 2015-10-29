9f7b9c4805ab98db5e0e2440d71527e7547700ab
hectorinoman <hectorinoman@gmail.com>
2015-10-29 15:58:33 +0000

    static-server

--- static_server.js
+++ static_server.js
@@ -2,7 +2,7 @@ var static = require('node-static');
 
 var fileServer = new static.Server('./');
 
-console.log("Visit http://localhost:8080/test/test_blanket.html")
+console.log("Visit http://10.6.128.89:8080/index.html")
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         fileServer.serve(request, response);
